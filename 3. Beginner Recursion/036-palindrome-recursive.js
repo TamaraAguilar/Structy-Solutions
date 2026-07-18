1b7e10691a1a06a7787798a57835cb2e87a926b1
TamaraAguilar <tamaraaguilar5@gmail.com>
2026-07-17 21:13:50 -0400

    feat: solved '035-palindrome recursive'

---
+++ 3. Beginner Recursion/036-palindrome-recursive.js
@@ -0,0 +1,16 @@
+const palindrome = (s) => {
+  // Base case
+  if (s.length <= 1) {
+    return true;
+  }
+
+  if (s[0] !== s[s.length - 1]) {
+    return false;
+  }
+
+  return palindrome(s.slice(1, -1));
+};
+
+module.exports = {
+  palindrome,
+};
