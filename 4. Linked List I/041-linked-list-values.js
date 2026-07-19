0d88fcffeed85a83304e2d97759bf59c01407e40
TamaraAguilar <tamaraaguilar5@gmail.com>
2026-07-18 21:59:22 -0400

    fix: add recursive solution for '041- linked list values'

--- 4. Linked List I/041-linked-list-values.js
+++ 4. Linked List I/041-linked-list-values.js
@@ -1,20 +1,35 @@
-class Node {
-  constructor(val) {
-    this.val = val;
-    this.next = null;
-  }
-}
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
 
+// Iterative solution
+// const linkedListValues = (head) => {
+//   let current = head;
+//   const result = [];
+
+//   while (current !== null) {
+//     result.push(current.val);
+//     current = current.next;
+//   }
+
+//   return result;
+// };
+
+// Recursive solution
 const linkedListValues = (head) => {
-  let current = head;
-  const result = [];
+  const values = [];
+  fillValues(head, values);
+  return values;
+};
 
-  while (current !== null) {
-    result.push(current.val);
-    current = current.next;
-  }
-
-  return result;
+const fillValues = (head, values) => {
+  // Base case
+  if (head === null) return;
+  values.push(head.val);
+  fillValues(head.next, values);
 };
 
 module.exports = {
