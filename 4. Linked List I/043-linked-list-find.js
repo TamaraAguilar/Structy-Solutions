71b3bf1e45681ad84532bfccbcdab1cc9e6e80ed
TamaraAguilar <tamaraaguilar5@gmail.com>
2026-07-19 21:31:52 -0400

    fix: add recursive solution

--- 4. Linked List I/043-linked-list-find.js
+++ 4. Linked List I/043-linked-list-find.js
@@ -17,12 +17,20 @@
 // };
 
 // Iterative solution
-const linkedListFind = (head, target) => {
-  // Base cases
-  if (head === null) return false;
-  if (head.val === target) return true;
+// const linkedListFind = (head, target) => {
+//   // Base cases
+//   if (head === null) return false;
+//   if (head.val === target) return true;
 
-  return linkedListFind(head.next, target);
+//   return linkedListFind(head.next, target);
+// };
+
+// Recursive solution
+const getNodeValue = (head, index) => {
+  if (head === null) return null;
+  if (index === 0) return head.val;
+
+  return getNodeValue(head.next, index - 1);
 };
 
 module.exports = {
